6aa3992f234ceff05932967b170171e7be556013
Meljska-Fawn <meli.explores@gmail.com>
2023-06-01 21:40:59 -0700

    updated server.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://meljska_fawn:gDP5ENri7u@cluster0.ow43r5j.mongodb.net/bookFinder?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://meli_explores@gmail.com:gDP5ENri7u@cluster0.ow43r5j.mongodb.net/bookFinder?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
