ed60260e7b783f6d6d2ca25ca760a4ad3cf6f933
Meljska-Fawn <meli.explores@gmail.com>
2023-06-01 19:17:38 -0700

    updated server package.json

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookFinder', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://meljska_fawn:gDP5ENri7u@cluster0.ow43r5j.mongodb.net/bookFinder?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
